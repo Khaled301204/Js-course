182cc50c6a44861f2ddf97fb5b9cc7c625b817ba
Khaled301204 <khaledharfoosh2014@gmail.com>
2024-09-20 22:52:43 +0300

    lec1

--- Testjs/test.js
+++ Testjs/test.js
@@ -15,14 +15,15 @@ const John_BMI = getBMI(John_mass, John_height);
 
 Mark_BMI>John_BMI?console.log("Mark has higher BMI"):console.log("John has higher BMI");
 */
-
 total = [];
+tips = [];
 function CalcTip(bill){
-    bill>50 && bill<300?total.push(bill*=0.15):total.push(bill*=0.20);
+    bill>50 && bill<300?tips.push(bill*0.15):tips.push(bill*0.20);
+    bill>50 && bill<300?total.push(bill+(bill*0.15)):total.push(bill+(bill*0.20));
 }
-CalcTip(25);
-CalcTip(200);
-CalcTip(400);
-CalcTip(140);
-CalcTip(850);
+CalcTip(125);
+CalcTip(555);
+CalcTip(44);
+
+console.log(tips);
 console.log(total);
